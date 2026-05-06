288940948c248e02607693486e3ce62a603417f6
JIHO0213 <julove0319@gmail.com>
2026-05-06 16:41:58 +0900

    교수님 풀이

--- c/5-1/5_1.c
+++ c/5-1/5_1.c
@@ -1,39 +1,78 @@
 #define _CRT_NO_SECURE_WARNING
 #include <stdio.h>
 
-
-int rev(int n){
+// 숫자 뒤집기
+int Rev(int n){
     int reversed = 0;
-    while (n>0){
-        reversed = reversed * 10 + n%10;
+    while (n > 0){
+        reversed = reversed * 10 + n % 10;
         n /= 10;
     }
     return reversed;
 }
-
-void prime_frac(int n){
+// 소인수 분해 후 출력
+void Prime_Frac_Print(int n){
     
-    for (int i =2; i<n; i++){
+    for (int i =2; i < n; i++){
         int count = 0;
-        while (n%i == 0){
+        while (n % i == 0){
             n /= i;
             count++;
         }
         if (count) printf("%d %d\n", i, count);
     }
-    if (n>1) printf("%d %d", n, 1);
+    if (n > 1) printf("%d %d", n, 1);
+    /*
+    while (n != 0){
+        for (int i =2; i<= n ; i++){
+            if (n % i == 0){
+                int cnt = 0;
+                while (1){
+                    n /= i;
+                    cnt++;
+                    if (n % i != 0) break;
+                }
+                printf("%d %d\n", i, cnt);
+            }
+        }    
+    }
+
+    24
+    2 12
+    2 6
+    2 3
+    3 1
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+
+    */
 }
 
-//24
+
+void main(){
+    // int n, reversed;
+    // scanf("%d", &n);
+
+    // reversed = Rev(n);
+    // printf("%d\n", reversed);
+    // Prime_Frac_Print(reversed);
 
 
-
-int main(){
-    int n;
-    scanf("%d", &n);
-
-    printf("%d\n",rev(n));
-    prime_frac(rev(n));
-
-    return 0;
-}
+    int n = 24;
+        for (int i =2; i<= n ; i++){
+            if (n % i == 0){
+                int cnt = 0;
+                while (1){
+                    n /= i;
+                    cnt++;
+                    if (n % i != 0) break;
+                }
+                printf("%d %d\n", i, cnt);
+            }
+        }    
+    }

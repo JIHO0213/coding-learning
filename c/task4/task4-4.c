99787d74e891306f55e8564134f14b54e9465e10
JIHO0213 <julove0319@gmail.com>
2026-05-13 02:09:07 +0900

    05012

--- c/task4/task4-4.c
+++ c/task4/task4-4.c
@@ -1,27 +1,42 @@
 #define _CRT_NO_SECURE_WARNING
 #include <stdio.h>
 
-// 35분 +
+// 1시간 + AI
+
 void main(){
-    int N,K;
-    int num[100];
-    int sorted[100];
-    scanf("%d %d", &N, &K);
-    for (int i=0; i<N; i++) scanf("%d", &num[i]);
-    
-    int count = (N%K) ? N/K+1 : N/K;
-    
-    int index = 0;
-    
-    int i = 0;
-    while (i < N){
-
-        if (num[i] > num[i+1]) {
-
+  int N,K;
+  int tmp;
+  int num[100];
+  scanf("%d %d", &N, &K);
+  for (int i = 0; i<N; i++) scanf("%d", &num[i]);
+  
+  // l을 k단위로 이동시켜 묶음 나눔
+  for (int l = 0; l<N; l += K){
+      // 인접한 두 값 중 큰 값을 끝으로 밀어냄
+      // -> 한 번의 반복(i)이 끝나면 가장 큰 값이 맨 뒤에 위치
+      // -> 이 과정을 N-1번 반복해야 정렬
+    for (int j = l; j<K-1+l; j++){
+        //i < N-1 조건 : 정렬이 배열 바깥에 넘어가지 않도록
+         for (int i = l; i<K-1+l && i<N-1; i++){ 
+             if (num[i] > num[i+1]) {tmp = num[i]; num[i] = num[i+1]; num[i+1] = tmp;}
+            } // num[i] > num[i+1]시 교환
         }
-        i += K;
     }
-
-        
+    for (int i = 0; i<N; i++) printf("%d ", num[i]);
 }
 
+
+/*
+참고) 버블 정렬
+for (int i = 0; i<N-1; i++){
+    for (int j = 0; j<N-1; j++){
+        if(arr[i] > arr[i+1]) {
+            tmp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = tmp;
+        }
+    }    
+}
+ - 구조가 단순
+ - 반복 횟수가 많아 시간이 오래 걸림
+*/
--- c/task4/task5.c
+++ c/task4/task5.c
@@ -1,30 +1,31 @@
 #define _CRT_NO_SECURE_WARNING
 #include <stdio.h>
-// 30분 + 
+// 35분 + 10분
 void main(){
     int N;
     int K;
     int num[100];
-    int result[100];
-
+    int tmp;
     scanf("%d", &N);
-    for (int i = 0; i<N;i++) scanf("%d", &num[i]);
-    scanf("%d", &K);
-
-    int count = 0;
-    for (int j = 0; j < N; j++){
-        if (num[j]%2 == 0){
-            for (int k = N-1; k>j; k--){
-                result[k+1] = num[k];
-            }
-            result[j+1] = K;
-            result[j] = num[j];
-            count++;
-            j += 2;
+    for (int i = 0; i<N;i++) {
+        scanf("%d", &tmp); //임시값에 저장
+        if (tmp%3 == 0) { //임시값이 3의 배수
+            N--; //최댓값과 인덱스 감소시키기
+            i--; // -> 배열 한칸 빼기 기능
+            continue;
         }
-        else result[j] = num[j];
+        num[i] = tmp;
+        if (num[i]%2 == 0 ) { //입력값이 짝수라면 
+            num[i+1] = -1; //다음값을 비워놓고
+            N++; // 최댓값과 인덱스 증가시키키
+            i++; // -> 다음 루프는 i+2부터
+        } 
     }
+    scanf("%d", &K);    
 
-    for (int i = 0; i<N+count;i++) printf("%d ", result[i]);
-    
+    for (int j = 0; j<N; j++){
+        if (num[j] == -1) num[j] = K;
+    } // 비운값(-1)만 K로 교체
+
+    for (int i = 0; i<N;i++) printf("%d ", num[i]);
 }

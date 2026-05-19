8f69f5f3da610d78d381ecc03d02530e351114ea
JIHO0213 <julove0319@gmail.com>
2026-05-19 19:50:15 +0900

    실습 11 끝

--- c/0518/0518.c
+++ c/0518/0518.c
@@ -1,15 +1,148 @@
-#define _CRT_NO_SECURE_WARNING
+#define _CRT_SECURE_NO_WARNING
 #include <stdio.h>
 
+int reverse_num(int);
 int del_dupnum(int);
 
-void main(){
-    del_dupnum(123456);
+void main() {
+    int N = 1;
+    int dp_rev_N;
+    int max = -1;
+    int max_N;
+    int diff;
+    while (N>0){
+        scanf("%d", &N);
+        if (N<0) break;
+        dp_rev_N = del_dupnum(reverse_num(N));
+        diff = (N > dp_rev_N) ? N- dp_rev_N : dp_rev_N - N;
+        printf(" %d", dp_rev_N);
+        if (max < diff) {max = diff; max_N = N;}
+        
+    } 
+    printf("\n%d %d", max_N, max);
 }
 
-int del_dupnum(int x){
-    
+int reverse_num(int N){
+    int reverse = 0;
+    while (N){
+        reverse = reverse * 10 + N%10;
+        N /= 10;
+    } return reverse;
 }
+int del_dupnum(int x) {
+    int dgt_cnt = 1;
+    int result = 0;
+    int visited[10] = {0};
+    int tmp = x;
+    int digit;
+    // 1. x의 가장 큰 자릿수에 맞는 10의 거듭제곱 구하기
+    // 예: tmp가 123이면 dgt_cnt는 100이 됩니다.
+    while (tmp >= 10) {
+        dgt_cnt *= 10;
+        tmp /= 10;
+    }
+
+    // 2. 앞에서부터 한 자리씩 추출하며 중복 검사
+    while (dgt_cnt > 0) {
+        digit = x / dgt_cnt; // 가장 앞자리 숫자 추출
+
+        // 이미 나온 숫자가 아니라면 결과에 추가
+        if (visited[digit] == 0) {
+            visited[digit] = 1;          // 사용되었다고 표시
+            result = result * 10 + digit; // 결과 정수 조립
+        }
+
+        x %= dgt_cnt;     // 방금 사용한 앞자리 잘라내기
+        dgt_cnt /= 10;    // 다음 자릿수로 이동
+    }
+
+    return result;
+}
+
+// int reverse_num(int N);
+// int is_palindromic(int N);
+// int del_digit(int N, int T);
+
+// int main(){
+//     int N, T;
+//     scanf("%d %d", &N, &T);
+
+//     printf("%d %d %d %d", N, reverse_num(N), is_palindromic(N), del_digit(N,T));
+
+//     return 0;
+// }
+
+// int reverse_num(int N){
+//     int reverse = 0;
+//     while (N){
+//         reverse = reverse * 10 + N%10;
+//         N /= 10;
+//     } return reverse;
+// }
+// int is_palindromic(int N){
+//     if (N == reverse_num(N)) return 1;
+//     return 0;
+// }
+
+
+
+// int del_digit(int N, int T){
+//     int Num[9];
+//     int indx = 0, result = 0;
+//     int tmp = N;
+//     while (tmp){indx++; tmp/= 10;}
+//     tmp = N;
+//     for (int i = 0; i<indx; i++){
+//         Num[indx-i-1] = tmp%10;
+//         tmp/= 10;
+//     }
+//     if (is_palindromic(N)){
+//         Num[indx-T] = -1;
+//         Num[T-1] = -1;
+//     }// T자리, rev(T)자리 삭제 수 출력
+//     else {
+//         Num[indx-T] = -1;
+//     }
+
+//     for (int i = 0; i<indx; i++) {
+//         //printf("%d\n", Num[i]);
+//         if (Num[i] != -1) result = result * 10 + Num[i];
+//     }
+//     return result;
+// }
+
+
+// int prime_number(int num);
+// int odd_number(int num);
+// int main(){
+//     int X[100];
+//     int indx = 0;
+//     int N = 1;
+//     while (N > 0){
+//         scanf("%d", &N);
+//         if (N<0) break;
+//         if (prime_number(N) || odd_number(N)){
+//             X[indx++] = N;
+//         }
+//     }
+//     if (!indx) {printf("0"); return 0;}
+//     for (int i = 0; i< indx; i++) printf("%d ", X[indx-i-1]);
+//     return 0;
+// }
+
+// int prime_number(int num){
+//     for (int i = 2; i*i<=num; i++){
+//         if (num%i == 0) return 0;
+//     }
+//     return 1;
+// }
+// int odd_number(int num){
+//     while (num){
+//         if ((num%10)%2 == 0) return 0;
+//         num /= 10;
+//     }
+//     return 1;
+// }
 
 // //8분
 
